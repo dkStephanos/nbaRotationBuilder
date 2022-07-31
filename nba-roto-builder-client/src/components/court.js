b1dd6d769ec3acebaf8c93ad27ae2d25fb80645e
dkStephanos <dkstephanos@gmail.com>
2022-07-31 16:48:05 -0400

    setting up locations

--- nba-roto-builder-client/src/components/court.js
+++ nba-roto-builder-client/src/components/court.js
@@ -1,41 +1,24 @@
-import { useDrop } from 'react-dnd';
 import { Paper } from '@mui/material';
+import Position from './position';
 import CourtPng from '../imgs/basketball-court.png';
-const style = {
-	height: '4rem',
-	width: '4rem',
-	color: 'white',
-	padding: '.3rem',
-	textAlign: 'center',
-	fontSize: '2.4rem',
-	lineHeight: 'normal',
-	float: 'left',
-	borderRadius: '50%',
-	position: 'absolute',
-	zIndex: '100',
-};
+
+const positions = [
+	{ name: 'Point Guard', abrv: 'PG' },
+	{ name: 'Shooting Guard', abrv: 'SG' },
+	{ name: 'Small Forward', abrv: 'SF' },
+	{ name: 'Power Forward', abrv: 'PF' },
+	{ name: 'Center', abrv: 'C' },
+];
 
 const position_styles = {
-	center: { top: '40%', left: '65%' },
+	PG: { top: '1%', left: '45%' },
+	SG: { top: '15%', left: '5%' },
+	SF: { top: '65%', left: '90%' },
+	PF: { top: '55%', left: '25%' },
+	C: { top: '40%', left: '65%' },
 };
 
 const Court = (props) => {
-	const [{ canDrop, isOver }, drop] = useDrop(() => ({
-		accept: 'player',
-		drop: (player) => console.log(player),
-		collect: (monitor) => ({
-			isOver: monitor.isOver(),
-			canDrop: monitor.canDrop(),
-		}),
-	}));
-	const isActive = canDrop && isOver;
-	let backgroundColor = '#222';
-	if (isActive) {
-		backgroundColor = 'darkpurple';
-	} else if (canDrop) {
-		backgroundColor = 'purple';
-	}
-
 	return (
 		<div style={{ display: 'inline-block', verticalAlign: 'bottom', width: '57%', float: 'left' }}>
 			<Paper
@@ -44,15 +27,12 @@ const Court = (props) => {
 					backgroundImage: `url(${CourtPng})`,
 					backgroundRepeat: 'no-repeat',
 					backgroundSize: 'contain',
-					height: '500px',
+					height: '420px',
 					position: 'relative',
 				}}>
-				<div
-					ref={drop}
-					style={{ ...style, ...position_styles['center'], backgroundColor }}
-					data-testid='dustbin'>
-					{isActive ? 'Release to drop' : 'C'}
-				</div>
+				{positions.map((pos) => (
+					<Position pos={pos} position_styles={position_styles} />
+				))}
 			</Paper>
 		</div>
 	);
