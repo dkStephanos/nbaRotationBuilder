e791ec8450e4b0859d8124526c3b2bab4bc7e0db
dkStephanos <dkstephanos@gmail.com>
2022-07-31 15:16:11 -0400

    setting up card

--- nba-roto-builder-client/src/components/player.js
+++ nba-roto-builder-client/src/components/player.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Card } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Player = (props) => {
 	const [error, setError] = useState(false);
@@ -15,6 +20,7 @@ const Player = (props) => {
 
 	const headshot = (
 		<object
+			style={{ height: '80px' }}
 			data={`https://nba-players.herokuapp.com/players/${props.player[3].split(' ')[1]}/${
 				props.player[3].split(' ')[0]
 			}`}
@@ -22,9 +28,18 @@ const Player = (props) => {
 			alt='headshot'></object>
 	);
 	return (
-		<Card>
-			{console.log(headshot)}
-			{error ? <img alt='error' src='error.jpg' /> : headshot}
+		<Card style={{ height: '200px', width: '200px' }}>
+			<CardContent>
+				<Typography variant='h7' component='div'>
+					{props.player[3]}
+				</Typography>
+			</CardContent>
+			<CardMedia>
+				{error ? <img style={{ height: '80px' }} alt='error' src='error.jpg' /> : headshot}
+			</CardMedia>
+			<CardActions>
+				<Button size='small'>View Stats</Button>
+			</CardActions>
 		</Card>
 	);
 };
