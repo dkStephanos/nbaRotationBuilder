7b83b05c0b9ba6048e3a41f5e6e16549b9fb6492
dkStephanos <dkstephanos@gmail.com>
2022-07-31 12:45:15 -0400

    adding nba_roto_builder django app

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'students',
+    'nba_roto_builder',
 ]
 
 MIDDLEWARE = [
---
+++ django_api/nba_roto_builder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NbaRotoBuilderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nba_roto_builder'
